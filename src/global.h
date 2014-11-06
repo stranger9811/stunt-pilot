4700c82fdf6e19ca077f45ef02f35fa66ed26fb8
tapan1911 <tapan1911@gmail.com>
2014-11-07 02:33:08 +0530

    code synced

--- src/global.h
+++ src/global.h
@@ -11,15 +11,17 @@
 
 #ifdef __APPLE__
 #include <OpenGL/OpenGL.h>
+# include "SDL.h"
 #include <GLUT/glut.h>
 #else
 # include <GL/gl.h>
+# include "SDL/SDL.h"
 # include <GL/glu.h>
 # include <GL/glut.h>
 #endif
 
 
-# include "SDL.h"
+
 # include "SDL/SDL_mixer.h"
 # include "SDL/SDL_image.h"
 
